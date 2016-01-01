1b8eb6e0d2ceafeace5064af4ddea3ffdd691153
Thomas Steinholz <tsteinholz@yahoo.com>
2016-01-01 15:50:29 -0500

    init commit

---
+++ desktop/src/com/laststandstudio/concpet/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.laststandstudio.concpet.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.laststandstudio.concpet.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
