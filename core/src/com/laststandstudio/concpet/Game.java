68da40ad77177b727bf41068de392d7c14a3c97d
Thomas Steinholz <tsteinholz@yahoo.com>
2016-01-01 19:13:34 -0500

    fixed the html launcher size

--- core/src/com/laststandstudio/concpet/Game.java
+++ core/src/com/laststandstudio/concpet/Game.java
@@ -18,10 +18,15 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
 	}
 }
--- html/src/com/laststandstudio/concpet/client/HtmlLauncher.java
+++ html/src/com/laststandstudio/concpet/client/HtmlLauncher.java
@@ -1,15 +1,17 @@
 package com.laststandstudio.concpet.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.user.client.Window;
 import com.laststandstudio.concpet.Game;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Window.getClientWidth(), Window.getClientHeight());
         }
 
         @Override
